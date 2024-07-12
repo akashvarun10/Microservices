a5b1e126d9f2ae394fd9423f296c3adf4338056a
Pemmaraju Venkat Akash Varun <pemmaraju.v@northeastern.edu>
2024-07-12 13:35:15 +0530

    Containerized the app and written manifest files

--- python/src/auth/server.py
+++ python/src/auth/server.py
@@ -40,6 +40,37 @@ def login():
             return createJWT(auth.username, os.getenv("JWT_SECRET_KEY"), True)
     else:
         return "Invalid credentials", 401
+    
+
+@server.route("/validate", methods=["POST"])
+def validate():
+    encoded_jwt = request.headers("Authorization")
+    if not encoded_jwt:
+        return "Missing Credentials", 401
+    
+    encoded_jwt = encoded_jwt.split(" ")[1]
+    try:
+        decoded = jwt.decode(encoded_jwt, os.getenv("JWT_SECRET_KEY"), algorithm=["HS256"])
+    except:
+        return "not authorized", 403
+    return decoded, 200
+    
+
+def createJWT(username, secret, authz):
+    return jwt.encode(
+        {
+            "username": username,
+            "exp": datetime.datetime.now(tz=datetime.timezone.utc)
+            + datetime.timedelta(days=1),
+            "iat": datetime.datetime.utcnow(),
+            "admin": authz
+        },
+        secret,
+        algorithm="HS256"
+    )
+
+if __name__ == "__main__":
+    server.run(host="0.0.0.0", port=5100)
 
 
 
