40a0eb5d457b391a6504ca91c16c7a6acda24491
Pemmaraju Venkat Akash Varun <pemmaraju.v@northeastern.edu>
2024-07-13 16:54:16 +0530

    Updated authentication files and added converting feature

--- python/src/auth/init.sql
+++ python/src/auth/init.sql
@@ -1,18 +1,20 @@
-CREATE USER 'auth_user''@''localhost' IDENTIFIED BY 'Aauth123';
-
+CREATE USER 'auth_user'@'localhost' IDENTIFIED BY 'akashvarunp123';
 
 CREATE DATABASE auth;
 
-GRANT ALL PRIVILEGES ON auth.* TO 'auth_user''@''localhost';
+GRANT ALL PRIVILEGES ON auth.* TO 'auth_user'@'localhost';
 
 USE auth;
 
-CREATE TABLE users (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL
+CREATE TABLE user (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  email VARCHAR(255) NOT NULL UNIQUE,
+  password VARCHAR(255) NOT NULL
 );
 
-INSERT INTO users (email, password) VALUES ('akashvarunp@gmail.com', 'Admin123');
+INSERT INTO user (email, password) VALUES ('akashvarunp@gmail.com', 'Admin123');
+
+  
+
 
 
